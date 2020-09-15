49ba01a7f4cc8977d540d6a051338bdf7f9827e9
dherliansyah <dikiherliansyahh@gmail.com>
2020-09-16 01:25:14 +0700

    commit

--- tugas3_2.js
+++ tugas3_2.js
@@ -3,20 +3,18 @@ const getMonth = (callback) => {
     let error = false;
     let month = ["January","February","March","April","May","June","July","August","September","October","November","December",];
     if (!error) {
-      callback(month);
+      callback(null,month);
     } else {
       callback(new Error("Sorry Data Not Found", []));
     }
   }, 4000);
 };
 
-const tampilBulan = (bulan) => {
+const tampilBulan = (cek,bulan) => {
   if (Array.isArray(bulan)) {
     bulan.map((element) => {
       console.log(element);
     });
-  } else {
-    console.log(bulan.message);
   }
 };
 
--- tugas3_5.js
+++ tugas3_5.js
@@ -1,13 +1,26 @@
-function palindrome(str) {
-    var x = str.length-1;
-        
-    if( x === 0 || str.length === 0){
-        return "Ini Palindrome"
-    }
-    if(str[0] === str[x]){
-        return palindrome(str.slice(1,x))
-    }return "Data bukan palindrome";
+// START PALINDROME
+// function palindrome(str) {
+//     var x = str.length-1;
+
+//     if( x === 0 || str.length === 0){
+//         return "Ini Palindrome"
+//     }
+//     if(str[0] === str[x]){
+//         return palindrome(str.slice(1,x))
+//     }return "Data bukan palindrome";
+// }
+
+//     console.log(palindrome("malam")) // true
+
+// START REVERSE
+
+const words = ["saya", "belajar", "javascript"];
+
+for (var i = words.length - 1; i >= 0; i--) {
+  console.log(words[i]);
 }
 
-    console.log(palindrome("malam")) // true
-    
+function reverseString(str) {
+  return str;
+}
+reverseString("hello");
